a7f90592f1557eb1904b148c235744d74cb26cdf
Danielzanch <127164005+Danielzanch@users.noreply.github.com>
2023-12-13 18:36:06 -0300

    adicionando tratativa de erro

--- main.js
+++ main.js
@@ -1,33 +1,3 @@
-// $(document).ready(function() {
-//     $('#link').click(function() {
-//         const endpoint = `https://api.github.com/users/ogiansouza`;
-//         const botao = (this);
-//         $.ajax(endpoint).done(function(resposta) {
-//             const nome = resposta.name;
-//             const username = resposta.login;
-//             const img_user = resposta.avatar_url;
-//             const repos = resposta.public_repos;
-//             const seguidores = resposta.followers;
-//             const seguindo = resposta.following;
-
-//             const nomeElement = `${nome}`;
-//             const usernomeElement = `${username}`;
-//             const foto = `${img_user}`;
-//             const reposElement = `${repos}`;
-//             const seguidoresElement = `${seguidores}`;
-//             const seguindoElement = `${seguindo}`;
-
-//             $('#nome_usuario').val(nomeElement);
-//             $('#username').val(usernomeElement);
-//             $('#avatar').val(foto);
-//             $('#repos').val(reposElement);
-//             $('#seguidores').val(seguidoresElement);
-//             $('#seguindo').val(seguindoElement);
-//             console.log(seguindoElement);
-//         })
-//     })
-// })
-
 document.addEventListener('DOMContentLoaded', function () {
 
     $('#link').click(function () {
@@ -40,18 +10,24 @@ document.addEventListener('DOMContentLoaded', function () {
         const followingElement = document.querySelector('#following');
         const linkElement = document.querySelector('#link');
 
-        fetch('https://api.github.com/users/danielzanch')
-            .then(function (res) {
-                return res.json();
-            })
+        fetch('https://api.github.com/users/ogiansouza')
+        .then(function (res) {
+            return res.json();
+        })
             .then(function (json) {
-                nameElement.innerText = json.name;
-                usernameElement.innerText = json.login;
-                avatarElement.src = json.avatar_url;
-                followersElement.innerText = json.followers;
-                followingElement.innerText = json.following;
-                repos.innerText = json.public_repos;
-                linkElement.href = json.html_url;
-            })
+            nameElement.innerText = json.name;
+            usernameElement.innerText = json.login;
+            avatarElement.src = json.avatar_url;
+            followersElement.innerText = json.followers;
+            followingElement.innerText = json.following;
+            repos.innerText = json.public_repos;
+            linkElement.href = json.html_url;
+        })
+        .catch(function(erro) {
+            alert("Ocorreu um erro durante o processo");
+        })
+        .finally (function() {
+            alert("Obrigado por usar nossa sistema de visualização de perfil do github! \n Veja a seguir o resultado da busca.")
+        })
     })
 })
